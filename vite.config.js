86b47bd614a9b0195ed134ba98508c6156972e0d
Tomas <tomasgenco9@gmail.com>
2025-04-11 22:32:39 -0300

    cambios en base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://tomasgenco.github.io/Tateti',
+  base: 'https://tomasgenco.github.io/Tateti/',
   plugins: [react()],
 })
